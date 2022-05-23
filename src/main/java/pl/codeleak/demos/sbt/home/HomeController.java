286b5efe126667ae24f3deb2cd451bcace4df047
Naveen <naveen.ccmsd@gmail.com>
2022-05-24 01:04:26 +0530

    adfa

--- src/main/java/pl/codeleak/demos/sbt/home/HomeController.java
+++ src/main/java/pl/codeleak/demos/sbt/home/HomeController.java
@@ -21,6 +21,15 @@ class HomeController {
         model.addAttribute("grapOnlineReports",grapOnlineReports);
         return "index";
     }
+    @GetMapping("/home")
+    String index1(Model model) {
+        model.addAttribute("now", LocalDateTime.now());
+        Map<String,Boolean> grapOnlineReports = new HashMap<>();
+        grapOnlineReports.put("checkInvalidData" , true);
+        grapOnlineReports.put("checkvalidData" , false);
+        model.addAttribute("grapOnlineReports",grapOnlineReports);
+        return "reports";
+    }
 
     @GetMapping("properties")
     @ResponseBody
